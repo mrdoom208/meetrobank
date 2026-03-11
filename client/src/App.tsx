819606c838a4584ea76fb29247eaa4c16b9d917b
mrdoom <karlformentera2@gmail.com>
2026-03-11 21:19:15 +0800

    browser routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/login.tsx";
 import RecoveryAccess from "./pages/recoverAccess.tsx";
 import NotFound from "./pages/notFound.tsx";
-
 function App() {
   return (
-    <BrowserRouter basename="/client">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/recoveraccess" element={<RecoveryAccess />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/recoveraccess" element={<RecoveryAccess />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
